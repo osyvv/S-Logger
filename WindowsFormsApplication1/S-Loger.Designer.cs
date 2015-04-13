07460d5d3b049cd6641debf38892b029707dfa6f
Domingo <osyvv@nate.com>
2015-04-13 21:21:24 +0900

    S-Logger 초기 생성
    초기생성

---
+++ WindowsFormsApplication1/S-Loger.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.projectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addDeviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.refreshDeviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.reportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblInputData = new System.Windows.Forms.Label();
+            this.lblOutputData = new System.Windows.Forms.Label();
+            this.lblMillisecond = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.projectToolStripMenuItem,
+            this.recordToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(812, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // projectToolStripMenuItem
+            // 
+            this.projectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addDeviceToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.refreshDeviceToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.projectToolStripMenuItem.Name = "projectToolStripMenuItem";
+            this.projectToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.projectToolStripMenuItem.Text = "Project";
+            // 
+            // addDeviceToolStripMenuItem
+            // 
+            this.addDeviceToolStripMenuItem.Name = "addDeviceToolStripMenuItem";
+            this.addDeviceToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.addDeviceToolStripMenuItem.Text = "Add Device";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(150, 6);
+            // 
+            // refreshDeviceToolStripMenuItem
+            // 
+            this.refreshDeviceToolStripMenuItem.Name = "refreshDeviceToolStripMenuItem";
+            this.refreshDeviceToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.refreshDeviceToolStripMenuItem.Text = "Refresh Device";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(150, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // recordToolStripMenuItem
+            // 
+            this.recordToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openDataToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.reportToolStripMenuItem});
+            this.recordToolStripMenuItem.Name = "recordToolStripMenuItem";
+            this.recordToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.recordToolStripMenuItem.Text = "Record";
+            // 
+            // openDataToolStripMenuItem
+            // 
+            this.openDataToolStripMenuItem.Name = "openDataToolStripMenuItem";
+            this.openDataToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.openDataToolStripMenuItem.Text = "Open Data Viewer";
+            this.openDataToolStripMenuItem.Click += new System.EventHandler(this.openDataToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(169, 6);
+            // 
+            // reportToolStripMenuItem
+            // 
+            this.reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            this.reportToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.reportToolStripMenuItem.Text = "Report";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(218, 493);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(343, 62);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "btnOpen";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblInputData
+            // 
+            this.lblInputData.AutoSize = true;
+            this.lblInputData.Location = new System.Drawing.Point(216, 468);
+            this.lblInputData.Name = "lblInputData";
+            this.lblInputData.Size = new System.Drawing.Size(70, 12);
+            this.lblInputData.TabIndex = 3;
+            this.lblInputData.Text = "lblInputData";
+            // 
+            // lblOutputData
+            // 
+            this.lblOutputData.AutoSize = true;
+            this.lblOutputData.Location = new System.Drawing.Point(344, 468);
+            this.lblOutputData.Name = "lblOutputData";
+            this.lblOutputData.Size = new System.Drawing.Size(79, 12);
+            this.lblOutputData.TabIndex = 3;
+            this.lblOutputData.Text = "lblOutputData";
+            // 
+            // lblMillisecond
+            // 
+            this.lblMillisecond.AutoSize = true;
+            this.lblMillisecond.Location = new System.Drawing.Point(478, 468);
+            this.lblMillisecond.Name = "lblMillisecond";
+            this.lblMillisecond.Size = new System.Drawing.Size(83, 12);
+            this.lblMillisecond.TabIndex = 3;
+            this.lblMillisecond.Text = "lblMillisecond";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(27, 108);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(754, 318);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 20F);
+            this.label1.Location = new System.Drawing.Point(277, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 27);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "S-Logger Monitering";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 585);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lblMillisecond);
+            this.Controls.Add(this.lblOutputData);
+            this.Controls.Add(this.lblInputData);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "S-Logger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem projectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addDeviceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem refreshDeviceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem reportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblInputData;
+        private System.Windows.Forms.Label lblOutputData;
+        private System.Windows.Forms.Label lblMillisecond;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/S-Loger.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static int number = 0;
+
+        const char STX = (char)0x02;
+        const char ETX = (char)0x03; //End Text [응답용Asc]
+        const char EOT = (char)0x04; //End of Text[요구용 Asc]
+        const char ENQ = (char)0x05; //Enquire[프레임시작코드]
+        const char ACK = (char)0x06; //Acknowledge[응답 시작]
+        const char NAK = (char)0x15; //Not Acknoledge[에러응답시작]
+
+        private static SerialPort m_serialPort;
+
+        private List<double> _valueList1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Form load");
+            CheckForIllegalCrossThreadCalls = false;
+            setChart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            number++;
+            Debug.WriteLine("Hello = " + number);
+        }
+
+        private void openDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Help Button
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Exit Button
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        /// 
+        /// 포트오픈
+        /// 
+        private void SerialPortOpen()
+        {
+
+            // m_serialPort = new SerialPort("COM3", 19200, Parity.None, 8, StopBits.One);
+            m_serialPort = new SerialPort();
+
+            m_serialPort.PortName = "COM3";             // TODO COM3 하드코딩
+            m_serialPort.BaudRate = 19200;
+            m_serialPort.Parity = Parity.None;
+            m_serialPort.DataBits = 8;
+            m_serialPort.StopBits = StopBits.One;
+            m_serialPort.Handshake = Handshake.None;
+            m_serialPort.ReadTimeout = 500;
+            m_serialPort.WriteTimeout = 500;
+            
+            m_serialPort.Open();
+            
+            btnOpen.Enabled = false;
+
+            new Thread(new ThreadStart(getTemper)).Start();
+        }
+
+        // 1초마다 데이터 가져오기
+        public void getTemper()
+        {
+            while(true) {
+                getData();
+                Thread.Sleep(1000);
+            }
+        }
+
+        /// 
+        /// 포트 종료
+        /// 
+        private void SerialPortClose()
+        {
+            m_serialPort.Close();
+            btnOpen.Enabled = true;
+        }
+
+        // 시리얼번호 요청
+        private void getSerial_Click(object sender, EventArgs e)
+        {
+            getData();
+        }
+
+        delegate void setOutputData(string output);
+
+        private void setOutput(string data)
+        {
+            if (lblOutputData.InvokeRequired)
+            {
+                setOutputData call = new setOutputData(setOutput);
+
+            }else{
+                lblOutputData.Text = data;
+            }
+        }
+        
+        // 데이터 가져오기
+        public void getData()
+        {
+            
+            DateTime t1 = DateTime.Now;
+            string strOutputData = "ATCD\n";
+            m_serialPort.Write(strOutputData);
+
+            setOutput(strOutputData);
+            
+            do
+            {
+                //데이타를 전부 PLC로 전송 하기 위함..
+            } while (m_serialPort.WriteBufferSize == 0);
+
+            string indata = DataRead();
+            
+            double temper = Convert.ToDouble(indata.Substring(5, 4));
+
+            Debug.WriteLine("temper = " + temper);
+
+            addData(temper);
+
+            TimeSpan span = DateTime.Now.Subtract(t1);
+            lblMillisecond.Text = span.Milliseconds.ToString();
+        }
+
+        //실행
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            DateTime t1 = DateTime.Now;
+            string strOutputData = ENQ + "00RSS01" + "07%MX0000" + EOT;
+            m_serialPort.Write(strOutputData);
+            lblOutputData.Text = strOutputData;
+            do
+            {
+                //데이타를 전부 PLC로 전송 하기 위함..
+            } while (m_serialPort.WriteBufferSize == 0);
+            
+            string indata = DataRead();
+
+            TimeSpan span = DateTime.Now.Subtract(t1);
+            lblMillisecond.Text = span.Milliseconds.ToString();
+        }
+ 
+        /// 
+        /// PLC로 부터 수신된 데이타를 가지고 온다
+        /// 
+        /// 
+        private string DataRead()
+        {
+            bool m_Next = false;
+            string strInData = string.Empty;
+            string strRetValue= string.Empty;
+        
+            DateTime start = DateTime.Now;
+            do
+            {
+                string msg = m_serialPort.ReadExisting();
+
+                strInData += msg;
+                //TODO : 데이타에 종료문자가 있으면...
+                if (msg.IndexOf(ETX) > 0)
+                {
+                    //TODO 데이타 처음에 정상 응답이 있으면
+                    if (strInData[0] == ACK)
+                    {
+                        //TODO 들어오는 데이타를 분석..[ETX(1)+국번(2)+비트읽기(3)+블륵수(2)]
+                        lblInputData.Text = strInData;//Test용
+                        strRetValue = strInData.Substring(8, strInData.Length - 9); //실제Data
+                        m_Next = true;
+                    }
+                    //TODO: 데이타에 비정상 응답이 들어오면..
+                    else if (strInData[0] == NAK)
+                    {
+                        lblInputData.Text = "NAK";
+                        strRetValue = "-1";
+                        m_Next = true;
+                    }
+                }
+                //DOTO : 응답이 없으면 0.5초간은 로프를둘면서 기다란다.
+                TimeSpan ts = DateTime.Now.Subtract(start);
+                if (ts.Milliseconds > 500)
+                {
+                    //lblInputData.Text = "TimeOut";
+                    //lblInputData.Text = strInData;
+
+                    strRetValue = "-3";
+                    m_Next = true;
+                }
+            } while (!m_Next);
+
+            //Debug.WriteLine("strInData = " + strInData);
+
+            return strInData;
+        }
+
+        // 연결
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            SerialPortOpen();
+        }
+
+        // 연결 종료
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            SerialPortClose();
+        }
+
+        // 그래프 초기 세팅
+        private void setChart()
+        {
+            chart1.ChartAreas[0].AxisX.IsStartedFromZero = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = false;
+            chart1.ChartAreas[0].AxisX.ScaleView.SizeType = DateTimeIntervalType.Seconds;
+            chart1.ChartAreas[0].AxisX.IntervalAutoMode = IntervalAutoMode.FixedCount;
+            chart1.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Seconds;
+            chart1.ChartAreas[0].AxisX.Interval = 0;
+            chart1.ChartAreas[0].AxisY.Minimum = 23; // 최하온도
+            chart1.ChartAreas[0].AxisY.Maximum = 30; // 최대온도
+            _valueList1 = new List<double>();
+            DateTime now = DateTime.Now;
+            chart1.ChartAreas[0].AxisX.Minimum = now.ToOADate();
+            chart1.ChartAreas[0].AxisX.Maximum = now.AddSeconds(60).ToOADate();
+        }
+
+        // Data 추가
+        private void addData(double data)
+        {
+            //_valueList1.Add(System.DateTime.Now.Second);
+
+            Debug.WriteLine("data = " + data);
+            Debug.WriteLine("chart1 = " + chart1);
+
+            if (chart1 == null)
+            {
+                return;
+            }
+
+            Debug.WriteLine("chart1.Series[0] = " + chart1.Series[0]);
+
+            _valueList1.Add(data);
+            DateTime now = DateTime.Now;
+
+            if (chart1.Series[0].Points.Count > 0)
+            {
+                while (chart1.Series[0].Points[0].XValue < now.AddSeconds(-60).ToOADate())
+                {
+                    chart1.Series[0].Points.RemoveAt(0);
+                    chart1.ChartAreas[0].AxisX.Minimum = chart1.Series[0].Points[0].XValue;
+                    chart1.ChartAreas[0].AxisX.Maximum = now.AddSeconds(0).ToOADate();
+                }
+            }
+            chart1.Series[0].Points.AddXY(now.ToOADate(), _valueList1[_valueList1.Count - 1]);
+            chart1.Invalidate();
+        }
+    }
+}
